1091250a10c3c7ee96ec700d5c61fcc2c8d62091
Vadim <vadtverdohlebov@mail.ru>
2022-10-04 21:04:21 +0300

    add server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const os = require('os')
-let res = os.platform()
-console.log(res)
+const app = require('express')()
+
+const PORT = 3000
+const HOST = 'localhost'
+
+app.get('/', (req, res) => {
+    res.status(200).type('text/plain')
+    res.send('Home page')
+})
+
+app.listen(PORT, HOST, function () {
+    console.log(`Server listens http://${HOST}:${PORT}`)
+  })
