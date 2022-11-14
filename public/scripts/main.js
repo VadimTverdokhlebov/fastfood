21cac17e561059db918d0f22c2e03b35f947567c
Vadim <vadtverdohlebov@mail.ru>
2022-11-14 09:47:18 +0300

    added the display

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,23 @@
 "use strict";
 
-const menuModalCategorylId = ["sizes", "breads", "vegetables", "sauces", "fillings", "sandwichDone"];
+const menuModalCategorylId = ["size", "bread", "vegetable", "sauce", "filling", "sandwichDone"];
 const basket = [];
+let componentsCustomSandwich = new Map();
 let customSandwich = {};
 let indexMenuModal = 0;
-let previousElementModalMenu = "sizes";
+let previousElementModalMenu = "size";
+let comps = {
+    size: "Размер",
+    bread: "Хлеб",
+    vegetable: "Овощи",
+    sauce: "Соус",
+    filling: "Начинка",
+};
 
 selectedProductCategory();
 showBasket();
 
-async function getJson(){
+async function getJson() {
     const url = 'http://localhost:8080/api';
     const response = await fetch(url);
     const result = await response.json();
@@ -19,7 +27,7 @@ async function getJson(){
 
 async function getMenuProduct() {
     const result = await getJson();
-    
+
     return result.menu;
 }
 
@@ -59,7 +67,7 @@ async function getElementCustomSandwich(keyProduct) {
 function selectedProductCategory() {
 
     let categoriesMenu = document.querySelectorAll('.categoryMenu');
-    let defaultCategoryId = "pizza";
+    let defaultCategoryId = "sandwiches";
     for (let category of categoriesMenu) {
         category.addEventListener('click', function () {
             showProducts(category.id);
@@ -68,8 +76,6 @@ function selectedProductCategory() {
 
     showProducts(defaultCategoryId);
 }
-
-
 
 async function showProducts(categoryId) {
 
@@ -86,8 +92,9 @@ async function showProducts(categoryId) {
             content.insertAdjacentHTML("afterbegin", /*html*/
                 `<div class="product">
                 <img class="foodLogo" src="i/markets/subway_logo.png">
-                <img class="foodPicture" src="${menu[key].image}">
-
+                <div class="foodPicture">
+                    <img src="${menu[key].image}">
+                </div>
                 <div class="foodName">${menu[key].name}</div>
 
                 <div class="foodLine">
@@ -242,6 +249,8 @@ async function addProductInBasket(id, category, quantity) {
 
     if (category.id == 'sandwiches') {
         customSandwich = product;
+        addProductIncustomSandwich("white-italian");
+        addProductIncustomSandwich("1x");
         showModalCreateSandwich();
     }
     else if (productInBasket == true) {
@@ -268,8 +277,9 @@ function getSumOrder() {
 
 function showModalCreateSandwich() {
 
+    let sum = getSumProductInCustomSandwich() + customSandwich.price;
     let div = document.createElement('div');
-    const defultProductMenuId = "sizes";
+    const defultProductMenuId = "size";
 
     div.id = "modalWindow";
 
@@ -283,14 +293,15 @@ function showModalCreateSandwich() {
                 <button id="backButton" onclick="manageModalButton (-1)">Назад</button>
                 <button id="forwardButton" onclick="manageModalButton (1)">Вперед</button>
                 <ul id="menuModal">
-                    <li  class="categoryMenuModal" id="sizes"><a>Размер</a></li>
-                    <li  class="categoryMenuModal" id="breads"><a>Хлеб</a></li>
-                    <li  class="categoryMenuModal" id="vegetables"><a>Овощи</a></li>
-                    <li  class="categoryMenuModal" id="sauces"><a>Соусы</a></li>
-                    <li  class="categoryMenuModal" id="fillings"><a>Начинка</a></li>
+                    <li  class="categoryMenuModal" id="size"><a>Размер</a></li>
+                    <li  class="categoryMenuModal" id="bread"><a>Хлеб</a></li>
+                    <li  class="categoryMenuModal" id="vegetable"><a>Овощи</a></li>
+                    <li  class="categoryMenuModal" id="sauce"><a>Соусы</a></li>
+                    <li  class="categoryMenuModal" id="filling"><a>Начинка</a></li>
                     <li onclick="showSandwichDone()" id="sandwichDone">Готово</li>
                 </ul>
                 <div id="productContainer"></div>
+                
             </div>
         </div>
     `);
@@ -299,11 +310,12 @@ function showModalCreateSandwich() {
     selectCategoryMenuModal();
 
 }
+
 function selectCategoryMenuModal() {
     let categories = document.querySelectorAll('.categoryMenuModal');
-    
+
     for (let category of categories) {
-        
+
         category.addEventListener('click', function () {
 
             showProductMenuModal(category.id);
@@ -329,7 +341,7 @@ function manageModalButton(step) {
 
     } else {
         showProductMenuModal(menuModalCategorylId[indexMenuModal]);
-        
+
     }
 }
 
@@ -347,32 +359,56 @@ function showModalButton() {
     }
 }
 
+
 function showSandwichDone() {
 
     productContainer.remove();
     indexMenuModal = menuModalCategorylId.findIndex(category => category === "sandwichDone");
-
     let div = document.createElement('div');
+    let sum = getSumProductInCustomSandwich() + customSandwich.price;
 
     div.id = "productContainer";
 
     menuModal.after(div);
 
     productContainer.insertAdjacentHTML("afterbegin", /*html*/`
-        <div>
-        <img src="${customSandwich.image}">
+    <div class="doneProductContainer">
 
-        <div>${customSandwich.name}</div>
+        <div class="customSandwichMenu">
+            <div class="customSandwichPreview">
+                <img class="foodPicture" src="${customSandwich.image}">
+            </div>
 
-        <p>Цена: ${customSandwich.price} руб.</p>
+            <div id="customSandwichContainer">
+                <p id="yourSandwichIsDone">Ваш сендвич готов!</p>
+            </div>
+        </div>
+            
+        <div id="orderCustomSandwich">
+            <div>${customSandwich.name}</div>
+            <p>Цена: ${sum} руб.</p>
+            <button class="buttonBuy" onclick="addCustomSandwichToBasket()">В КОРЗИНУ</button>
+        </div>
 
     </div>
     `);
+    showComponentCustomSandwich();
+
     showModalButton();
     document.getElementById('sandwichDone').style.background = 'rgb(235, 74, 52)';
     document.getElementById(previousElementModalMenu).style.background = 'white';
 
     previousElementModalMenu = 'sandwichDone';
+}
+
+function addCustomSandwichToBasket() {
+    customSandwich.id = new Date().getTime();
+    customSandwich.name = customSandwich.name + " custom";
+    customSandwich.price = customSandwich.price + Number(getSumProductInCustomSandwich());
+
+    basket.push(customSandwich);
+    removeModalCreateSandwich();
+    showBasket();
 }
 
 async function showProductMenuModal(categoryId) {
@@ -387,12 +423,14 @@ async function showProductMenuModal(categoryId) {
     div.id = "productContainer";
 
     menuModal.after(div);
-   
+
     for (let key in products) {
 
         productContainer.insertAdjacentHTML("afterbegin", /*html*/`
         <div class="modalProductCard" id="${key}">
-        <img src="${products[key].image}">
+        <div class="foodPicture">
+            <img src="${products[key].image}">
+        </div>
 
         <div>${products[key].name}</div>
 
@@ -411,13 +449,14 @@ async function showProductMenuModal(categoryId) {
 
 function removeModalCreateSandwich() {
     modalWindow.remove();
+    componentsCustomSandwich = new Map();
 }
 
 function selectProductModal() {
     let products = document.querySelectorAll('.modalProductCard');
-    
+
     for (let product of products) {
-        
+
         product.addEventListener('click', function () {
 
             addProductIncustomSandwich(product.id);
@@ -426,13 +465,106 @@ function selectProductModal() {
     }
 }
 
+function getSumProductInCustomSandwich() {
+    let components = customSandwich.components;
+    let sum = 0;
+    for (let keyCategory in components) {
+        for (let keyProduct in components[keyCategory]) {
+            if (components[keyCategory][keyProduct].price) {
+                sum += components[keyCategory][keyProduct].price;
+            }
+        }
+    }
+    return sum;
+}
+
 async function addProductIncustomSandwich(id) {
     let product = await getElementCustomSandwich(id);
-    
-    customSandwich.components = Object.assign(customSandwich.components, product);
-    let map = new Map(Object.entries(customSandwich));
-    //console.log(product);
-    console.log(customSandwich);
-    console.log(map);
+
+    if (checkComponentCustomSandwich(id)) {
+        let map = new Map(Object.entries(product));
+        for (let key of map.keys()) {
+            map.set(key, []);
+        }
+        product = Object.fromEntries(map.entries());
+        customSandwich.components = Object.assign(customSandwich.components, product);
+
+    } else {
+
+        customSandwich.components = Object.assign(customSandwich.components, product);
+    }
+
+    if (Object.keys(customSandwich.components.bread).length == 0) {
+        let defaultBread = await getElementCustomSandwich("white-italian");
+        customSandwich.components = Object.assign(customSandwich.components, defaultBread);
+    }
+
+    if (Object.keys(customSandwich.components.size).length == 0) {
+        let defaultSize = await getElementCustomSandwich("1x");
+        customSandwich.components = Object.assign(customSandwich.components, defaultSize);
+    }
+
+    componentsCustomSandwich = new Map();
 
 }
+
+function checkComponentCustomSandwich(id) {
+    let map = new Map(Object.entries(customSandwich.components));
+    for (let value of map.values()) {
+        let component = Object.assign(value);
+        for (let key in component) {
+            if (key === id) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function showComponentCustomSandwich() {
+
+    addComponentCustomSandwich();
+    addEmptyComponentsCustomSandwich();
+
+    componentsCustomSandwich.forEach((value, key) => {
+        customSandwichContainer.insertAdjacentHTML("beforeend", /*html*/`
+        <div class="listComponentSandwiches">${key}: ${value}</div>`);
+    });
+}
+
+function addEmptyComponentsCustomSandwich() {
+
+    for (let key in comps) {
+        if (!checkCategoryCustomSandwich(comps[key])) {
+            componentsCustomSandwich.set(comps[key], "Нет");
+        }
+    }
+}
+
+function addComponentCustomSandwich() {
+
+    let currentComponents = customSandwich.components;
+
+    for (let key in currentComponents) {
+        for (let keyCategory in currentComponents[key]) {
+            for (let keyComps in comps) {
+                if (key === keyComps) {
+                    componentsCustomSandwich.set(comps[keyComps], currentComponents[key][keyCategory].name);
+                }
+            }
+        }
+    }
+}
+
+function checkCategoryCustomSandwich(currentKey) {
+
+    for (let keyCategory of componentsCustomSandwich) {
+        if (currentKey === keyCategory[0] && keyCategory[1] != undefined) {
+
+            return true;
+        }
+    }
+    return false;
+}
+
+
